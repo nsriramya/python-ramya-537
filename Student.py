3d77d95f4da2acd0dc7a563665df9a50565f5b4f
NALLAMSETTY SRI RAMYA <71599848+nsriramya@users.noreply.github.com>
2021-12-16 22:11:03 +0530

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,14 @@
+class Student:
+    wh=4
+    def __init__(self,c,a):
+         self.m=10
+         self.name="titrN"
+          
+    
+print(Student.wh)       
+s1=Student(10,"bf")
+s2=Student(90,"fdg")
+print(s1.m,s1.name)
+s2.name="wddvwe"
+print(s2.m,s2.name)
+ 
